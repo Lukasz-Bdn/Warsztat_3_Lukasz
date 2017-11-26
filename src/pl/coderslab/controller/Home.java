8c7b0608ec4b5fc54e504558f43d56cbe6f81234
Lukasz.Budyn <lukasz-budyn@wp.pl>
2017-11-26 14:48:56 +0100

    Updates in context.xml to handle database, Home.java and DBUtil as well

--- src/pl/coderslab/controller/Home.java
+++ src/pl/coderslab/controller/Home.java
@@ -1,11 +1,21 @@
 package pl.coderslab.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import pl.coderslab.model.Solution;
+import pl.coderslab.model.SolutionDao;
+import pl.coderslab.model.SolutionWithUser;
+import pl.coderslab.warsztat3.db.DbUtil;
 
 /**
  * Servlet implementation class Home
@@ -28,6 +38,9 @@ public class Home extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int count = Integer.parseInt(getServletContext().getInitParameter("number-solutions"));
 		response.getWriter().println(count);
+		List<SolutionWithUser> solutions = Solution.loadAllWithUser(count);
+		request.setAttribute("solutions", solutions);
+		getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
 	}
 
 	/**
